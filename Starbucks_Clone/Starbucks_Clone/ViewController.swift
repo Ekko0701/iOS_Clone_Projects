ceefbb56046d1727603870ad7655317996a99827
Ekko0701 <djekko0701@gmail.com>
2022-06-28 21:34:29 +0900

    Starbucks commit

---
+++ Starbucks_Clone/Starbucks_Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Starbucks_Clone
+//
+//  Created by Corder Collie on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("스타벅스 앱 클론코딩 시작!")
+    }
+
+
+}
+
